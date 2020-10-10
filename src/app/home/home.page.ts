c1043bbf32fbff5ef67755298e82d16a03655772
Michał Czyż <michal.czyz35@gmail.com>
2020-10-10 23:19:16 +0200

    Design Part 3

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TodosService } from '../services/todos.service';
+import { IonReorderGroup } from '@ionic/angular';
+import { ItemReorderEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,8 @@ import { TodosService } from '../services/todos.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit{
+
+  @ViewChild(IonReorderGroup) reorderGroup: IonReorderGroup;
 
   constructor(private todosservice: TodosService) {}
 
@@ -22,6 +26,17 @@ export class HomePage implements OnInit{
     this.todosservice.findLastCreatedTodos(this.todoLimit).subscribe(res => {
       this.lastCreatedFiveTodos = res;
     });
+  }
+
+  doReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
   }
 
 }
